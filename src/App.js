38a42f1389b36f9c91140ce1ddc719fbd00ca8cc
Kasper Peulen <kasperpeulen@gmail.com>
2018-12-27 12:25:39 +0100

    add bootstrap, prettier and add a textarea element

--- src/App.js
+++ src/App.js
@@ -4,7 +4,27 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App"></div>
+      <div>
+        <div
+          className="App"
+          style={{
+            width: '100vw',
+            height: '100vh',
+            padding: '20px'
+          }}
+        >
+          <textarea
+            style={{
+              height: '100%',
+              width: '100%',
+              resize: 'none',
+              border: 'none',
+              outline: 'none'
+            }}
+            placeholder={'Start typing...'}
+          />
+        </div>
+      </div>
     );
   }
 }
